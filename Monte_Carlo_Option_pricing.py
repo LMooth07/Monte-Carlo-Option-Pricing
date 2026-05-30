7f586498e035e183b4ca280ab9d21f68ac90021d
lucmoothia <lucmoothia@icloud.com>
2026-05-30 22:40:02 +0100

    Upload of Monte Carlo option pricing code

---
+++ Monte_Carlo_Option_pricing.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 30 00:58:52 2026
+
+@author: lucmoothia
+"""
+
+import numpy as np 
+import matplotlib.pyplot as plt
+import yfinance as yf
+from scipy.stats import norm
+
+
+
+
+
+
+def simulation (s0, r, sig, T, N):
+    dt = T/N
+    prices = [s0]
+    for i in range (N):
+        Z = np.random.normal()
+        old_price = prices[-1]
+        prices.append(old_price + r * old_price * dt + sig * old_price * np.sqrt(dt) * Z)
+    plt.plot(prices)
+    plt.xlabel('Days from s0')
+    plt.ylabel('Stock Price')
+    plt.title('Random Stock Price Simulation')
+
+  
+      
+def repeated_simulation (s0, r, sig, T, N, M) :
+    dt = T/N
+    for j in range(M):
+        prices = [s0]
+        for i in range (N):
+            Z = np.random.normal()
+            old_price = prices[-1]
+            prices.append(old_price + r * old_price * dt + sig * old_price * np.sqrt(dt) * Z)
+        plt.plot (prices, linewidth='0.5')
+    plt.xlabel('Days from s0')
+    plt.ylabel('Stock Price')
+    plt.title('Multiple Random Stock Price Simulation')
+ 
+    
+ 
+def option_price (s0, k, r, sig, T, N, M) :
+    payoffs = []
+    dt = T/N
+    for j in range(M):
+        prices = [s0]
+        for i in range (N):
+            Z = np.random.normal()
+            old_price = prices[-1]
+            prices.append(old_price + r * old_price * dt + sig * old_price * np.sqrt(dt) * Z)
+        St = prices[-1]
+        payoff=max(St - k,0)
+        payoffs.append(payoff)
+    option_price = np.exp(-r * T) * np.mean(payoffs)
+    return option_price
+
+
+
+def black_scholes_call(S0, K, r, sig, T):
+    d1 = (np.log(S0 / K) + (r + 0.5 * sig**2) * T) / (sig * np.sqrt(T))
+    d2 = d1 - sig * np.sqrt(T)
+    return S0 * norm.cdf(d1) - K * np.exp(-r * T) * norm.cdf(d2)
+
+
+
+
+
+data = yf.download('AAPL',
+                   start = '2020-01-01',
+                   end = '2025-01-01')
+
+def AAPL_stock_plot ():
+    data["Close"].plot()
+    plt.title('Apple Stock Price')
+    
+data["Returns"] = np.log(data["Close"]/data["Close"].shift(1))
+returns = data["Returns"].dropna()
+
+mu_daily = returns.mean()
+sigma_daily = returns.std()
+mu = mu_daily * 252
+sigma = sigma_daily * np.sqrt(252)
+
+
+
+            
+            
+            
+            
+            
